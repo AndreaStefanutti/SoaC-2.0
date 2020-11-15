d49039c22a7e301f5795fc40e5dffc5cef354548
davide orienti <dav.orienti@stud.uniroma3.it>
2020-11-15 18:14:08 +0100

    Correzione Mira/Sparo
    Spara solo quando l arma è in mano e si accende il mirino

--- SoaC/Assets/Script/Sparare/TPSShooter.cs
+++ SoaC/Assets/Script/Sparare/TPSShooter.cs
@@ -7,7 +7,7 @@ using System.Collections;
 
 public class TPSShooter : MonoBehaviour
 {
-    
+
     [SerializeField]
     GameObject projectile;
 
@@ -25,43 +25,58 @@ public class TPSShooter : MonoBehaviour
 
     LayerMask hittableMask;
     LayerMask mapMask;
+
+    public GameObject Player;
+    public PickUpController equipaggiato;
+    public bool equiped;
+
     void Start()
     {
+        // Player = Player;
         hittableMask = (1 << LayerMask.NameToLayer("Default"));
         mapMask = (1 << LayerMask.NameToLayer("Map")) | hittableMask;
+
+        equiped = equipaggiato.equipped;
+
+
     }
     void Update()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, distance, mapMask, QueryTriggerInteraction.Ignore))
-            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, distance, mapMask, QueryTriggerInteraction.Ignore))
-            {
-            lineRenderer.SetPosition(1, lineRenderer.transform.InverseTransformPoint(hit.point));
-        }
-       // if (Input.GetButtonDown(Sparo))
-       if (Input.GetMouseButtonDown(0))
+        equiped = equipaggiato.equipped;
+        if (equiped)
         {
-            GameObject projectileInstance = Instantiate(projectile, lineRenderer.transform.position, Quaternion.identity);
-
-            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, distance, hittableMask, QueryTriggerInteraction.Ignore))
+            RaycastHit hit;
+            if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, distance, mapMask, QueryTriggerInteraction.Ignore))
+                if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, distance, mapMask, QueryTriggerInteraction.Ignore))
+                {
+                    lineRenderer.SetPosition(1, lineRenderer.transform.InverseTransformPoint(hit.point));
+                }
+            // if (Input.GetButtonDown(Sparo))
+            if (Input.GetMouseButtonDown(0))
             {
-               projectileInstance.transform.LookAt(hit.point);
+                GameObject projectileInstance = Instantiate(projectile, lineRenderer.transform.position, Quaternion.identity);
 
-                Rigidbody colRigidbody = hit.collider.gameObject.GetComponent<Rigidbody>();
-                if (!colRigidbody) { return; }
-                colRigidbody.isKinematic = false;
-                colRigidbody.AddForceAtPosition(camTransform.forward * projectileForce, hit.point, ForceMode.Impulse);
-                Debug.LogError("Hai colpito: " + hit.collider.gameObject);
+                if (Physics.Raycast(camTransform.position, camTransform.forward, out hit, distance, hittableMask, QueryTriggerInteraction.Ignore))
+                {
+                    projectileInstance.transform.LookAt(hit.point);
+
+                    Rigidbody colRigidbody = hit.collider.gameObject.GetComponent<Rigidbody>();
+                    if (!colRigidbody) { return; }
+                    colRigidbody.isKinematic = false;
+                    colRigidbody.AddForceAtPosition(camTransform.forward * projectileForce, hit.point, ForceMode.Impulse);
+                    Debug.LogError("Hai colpito: " + hit.collider.gameObject);
+                }
+
+                else
+                {
+                    Vector3 lineRendererWorldEndPoint = lineRenderer.transform.TransformPoint(lineRenderer.GetPosition(1));
+                    projectileInstance.transform.LookAt(lineRendererWorldEndPoint);
+                }
+
+                Rigidbody projectileBody = projectileInstance.GetComponent<Rigidbody>();
+                projectileBody.AddForce(camTransform.forward * projectileForce, ForceMode.Impulse);
             }
-            
-            else
-            {
-                Vector3 lineRendererWorldEndPoint = lineRenderer.transform.TransformPoint(lineRenderer.GetPosition(1));
-                projectileInstance.transform.LookAt(lineRendererWorldEndPoint);
-            }
-            
-            Rigidbody projectileBody = projectileInstance.GetComponent<Rigidbody>();
-            projectileBody.AddForce(camTransform.forward * projectileForce, ForceMode.Impulse);
+
         }
     }
 }
