685128d1ebda007bded7c4cf84bbe895c560bb63
davide orienti <dav.orienti@stud.uniroma3.it>
2020-12-21 19:48:59 +0100

    Animazione zombi morte
    Abbiamo ucciso tutti gli zombi

--- SoaC/Assets/Script/Danno/DannoNemico.cs
+++ SoaC/Assets/Script/Danno/DannoNemico.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class DannoNemico : MonoBehaviour
 {
-    public float vita=100;
+    public float TempoMorte;
+    public float vita = 100;
+    public Animator anim;
+    void Start()
+    {
+       
+        anim = GetComponent<Animator>();
+        anim.SetBool("Death", false);
+
+    }
+
     public void Update()
     {
         if (vita <= 0)
         {
-            Destroy(gameObject);
-            Debug.Log("E morto");
+            StartCoroutine(wait());
         }
     }
-    public void getDanno(float danno)
-    {
-        vita = vita - danno;
-    }
+      public void getDanno(float danno)
+        {
+            vita = vita - danno;
+        }
+    
+        IEnumerator wait()
+        {
+        anim.SetBool("Death", true);
+        yield return new WaitForSeconds(TempoMorte);
+
+            Destroy(gameObject);
+            Debug.Log("E morto");
+
+
+
+        }
+
+    
 }
