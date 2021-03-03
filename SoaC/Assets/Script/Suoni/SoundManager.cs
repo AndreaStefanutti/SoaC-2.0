2816a9dcace6f3e9a7a864e3065d25d4488044ec
Andrea <and.stefanutti@stud.uniroma3.it>
2021-03-03 15:10:00 +0100

    Suono on off
    Suono on off con tasto che cambia nella scena opzioni

---
+++ SoaC/Assets/Script/Suoni/SoundManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Image soundOnIcon;
+    [SerializeField] Image soundOffIcon;
+    private bool muted = false;
+
+    void Start()
+    {
+        AggiornaBottone();
+        AudioListener.pause = muted;
+    }
+
+    private void Update()
+    {
+       
+    }
+
+    public void BottonePremuto()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+        AggiornaBottone();
+    }
+
+    private void AggiornaBottone()
+    {
+        if (muted == false)
+        {
+            soundOnIcon.enabled = true;
+            soundOffIcon.enabled = false;
+        }
+
+        else
+        {
+            soundOnIcon.enabled = false;
+            soundOffIcon.enabled = true;
+        }
+    }
+
+
+}
