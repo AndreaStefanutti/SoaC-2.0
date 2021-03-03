4bbab4ba7c600deb739f1389a481928f55cf729c
Andrea <and.stefanutti@stud.uniroma3.it>
2021-03-03 11:57:27 +0100

    Game over
    Stop dei movimenti alla morte

---
+++ SoaC/Assets/Script/Danno/DannoPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DannoPlayer : MonoBehaviour
+{
+    public float TempoMorte;
+    public int Vita;
+    int danno = 20;
+    public Animator anim;
+    public GameObject Controller;
+    GameObject Trasform;
+    public void Update()
+    {
+        if (Vita <= 0)
+        {
+            anim.SetBool("isDeath", true);
+            StartCoroutine(wait());
+        }
+    }
+    private void Start()
+    {
+        Vita = 100;
+        print(Vita);
+        anim = GetComponent<Animator>();
+        anim.SetBool("isDeath", false);
+        //Trasform = GetComponent<MyPlayer>
+    }
+    private void OnCollisionEnter(Collision collisione)
+    {
+        if (collisione.gameObject.tag == "Enemy")
+        {
+            Vita = Vita - danno;
+            print("Ecco la vita" + Vita);
+            Debug.LogWarning("Ecco la vita" + Vita);
+        }
+    }
+    IEnumerator wait()
+    {
+        Controller.SetActive(false);
+        GetComponent<MyPlayer>().SpeedVelocity = 0f;
+        anim.SetBool("Death", true);
+        yield return new WaitForSeconds(TempoMorte);
+
+        Destroy(gameObject);
+        Debug.Log("E morto");
+    }
+}
---
+++ SoaC/Assets/Script/UI/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void Gioco(string Gioco)
+    {
+        SceneManager.LoadScene(Gioco);
+
+    }
+    public void Home(string Home)
+    {
+        SceneManager.LoadScene(Home);
+
+
+    }
+}
